b5e9c16c6bb4dee286081c4455fa01593a6de637
Aleksander Gajewski <adiog@brainfuck.pl>
2017-10-22 16:30:29 +0200

    fix local ip

--- docker/async/docker.run.sh
+++ docker/async/docker.run.sh
@@ -2,10 +2,12 @@
 # This file is a part of quicksave project.
 # Copyright 2017 Aleksander Gajewski <adiog@quicksave.io>.
 
+HOSTIP=`ip route get 8.8.8.8 | awk '{print $NF; exit}'`
+
 sudo docker rm async_quicksave_docker
 sudo docker run \
     --privileged \
         --name async_quicksave_docker \
         --net network_quicksave_docker \
-        --add-host audit.quicksave.io:192.168.0.1 \
+        --add-host audit.quicksave.io:${HOSTIP} \
         -it quicksave/test-async
